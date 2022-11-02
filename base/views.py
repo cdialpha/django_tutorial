9ecaa00c956b7c78d4e34dde774daf30d7b99cca
cdialpha <cdialpha@gmail.com>
2022-11-02 14:29:12 -0400

    add comment functionality (CRU no D)

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect 
 from django.contrib import messages
-from .models import Room, Topic 
+from .models import Room, Topic, Message 
 from .forms import RoomForm
 from django.db.models import Q 
 from django.contrib.auth.models import User
@@ -74,7 +74,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
-    context = {'room' : room} 
+    room_messages = room.message_set.all().order_by('-create')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body'),
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk = room.id)
+
+    context = {'room' : room, 'room_messages' :room_messages, "participants" : participants } 
     return render(request, 'base/room.html', context)
 
 
@@ -122,4 +134,18 @@ def deleteRoom(request, pk):
         room.delete() 
         return redirect('home')
     context = {'obj': room }
+    return render(request, 'base/delete.html', context)
+
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Only authors can delete their own messages')
+    
+    if(request.method == 'POST'): 
+        message.delete() 
+        return redirect('home')
+    context = {'obj': message }
     return render(request, 'base/delete.html', context)
